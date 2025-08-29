b085a932dc785a43302b8a2ece6402dbc7701823
Gopi <208a1a0543@gmail.com>
2025-08-29 18:18:29 +0530

    changing

--- port/script.js
+++ port/script.js
@@ -15,3 +15,17 @@ window.addEventListener("scroll", function(){
 if (window.scrollY < 300) {
   backTopbtn.classList.remove("active");
 }
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.body.classList.toggle("dark-mode");
+
+  // Change button text dynamically
+  if (document.body.classList.contains("dark-mode")) {
+    themeToggle.textContent = "Light Mode";
+  } else {
+    themeToggle.textContent = "Dark Mode";
+  }
+});
